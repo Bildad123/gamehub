7f6a2442794bf6e21fc32f73756bde75be0b371d
Nyanga Bildad <n.bildad@ostfalia.de>
2023-08-06 18:48:14 +0200

    Create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Typography } from "@mui/material";
-
-interface Game {
-  id: number;
-  name: String;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState<String>("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGameResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        setError(err.message);
-        console.log(err);
-      });
-  }, []);
+  const { games, error } = useGames();
   return (
     <>
       {error && <Typography>{error}</Typography>}
