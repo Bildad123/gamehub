86032dab7ef58e02c483a46c29604ce91df04175
Nyanga Bildad <n.bildad@ostfalia.de>
2023-08-11 20:54:27 +0200

    Displaying Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,27 @@
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import useGenre from "../hooks/useGenre";
 
 const GenreList = () => {
   const { data } = useGenre();
   return (
     <>
-      <ul>
+      <List>
         {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id}>
+            <ListItemAvatar>
+              <Avatar src={genre.image_background} />
+            </ListItemAvatar>
+            <ListItemText primary={<Typography> {genre.name} </Typography>} />
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
